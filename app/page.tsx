c161521e5444ac4578544ee8510c588a2a55efbc
carlos alberto <c.morais1507@gmail.com>
2023-08-10 22:30:11 -0300

    :sparkles: feat: Base of the project

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,21 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="flex min-h-screen flex-col items-center px-24 py-12">
+      <h1 className='text-3xl font-bold text-primary uppercase mb-10'>Checkout</h1>
+      <div className='w-full flex'>
+        <div className='w-1/2 p-3'>
+          <div className='bg-base-200 border-neutral h-[650px] w-full rounded-xl'>
+
+          </div>
+        </div>
+        <div className='w-1/2 p-3'>
+          <div className='bg-base-200 border-neutral h-[550px] w-full rounded-xl'>
+
+          </div>
+          <button className='btn btn-secondary mt-5 btn-disabled'>Pay with Paypal</button>
+        </div>
+      </div>
     </main>
   )
 }
