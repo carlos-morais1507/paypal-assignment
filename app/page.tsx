bee70c2a7f40ff469aecd0abdd692dd687e37ed6
carlos alberto <c.morais1507@gmail.com>
2023-08-11 11:12:24 -0300

    :iphone: Responsive Design

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,20 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center px-24 py-12">
+    <main className="flex min-h-screen flex-col items-center p-6">
       <h1 className='text-3xl font-bold text-primary uppercase mb-10'>Checkout</h1>
-      <div className='w-full flex'>
-        <div className='w-1/2 p-3'>
-          <div className='bg-base-200 border-neutral h-[650px] w-full rounded-xl p-3'>
+      <div className='w-full flex flex-col md:flex-row'>
+        <div className='md:w-1/2 md:h-full p-3 w-full h-1/2'>
+          <div className='bg-base-200 border-neutral h-[650px] w-full rounded-xl p-3 overflow-scroll overflow-x-hidden flex md:flex-col gap-3 flex-wrap md:flex-nowrap mx-auto'>
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
             <ProductCard />
           </div>
         </div>
-        <div className='w-1/2 p-3'>
+        <div className='md:w-1/2 md:h-full p-3 w-full h-1/2'>
           <div className='bg-base-200 border-neutral h-[550px] w-full rounded-xl'>
           </div>
           <button className='btn btn-secondary mt-5 btn-disabled'>Pay with Paypal</button>
