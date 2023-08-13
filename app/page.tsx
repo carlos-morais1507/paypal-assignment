c27f7bef102118212a93f9c8895cf06ee46a34aa
carlos alberto <c.morais1507@gmail.com>
2023-08-13 00:38:06 -0300

    :heavy_plus_sign: chore: Add Paypal Dependencie

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import ProductGroup from '@/components/ProductGroup';
 import UserForm from '@/components/UserForm'
 
+declare const paypal: any;
+
 export default function Home() {
+
+  
+
   return (
     <main className="flex min-h-screen flex-col items-center p-6">
       <h1 className='text-3xl font-bold text-primary uppercase mb-10'>Checkout</h1>
@@ -12,7 +17,7 @@ export default function Home() {
           <div className='bg-base-200 border-neutral w-full rounded-xl'>
             <UserForm />
           </div>
-          <button className='btn btn-secondary mt-5'>Pay with Paypal</button>
+          <div id="paypal-button-container"></div>
         </div>
       </div>
     </main>
