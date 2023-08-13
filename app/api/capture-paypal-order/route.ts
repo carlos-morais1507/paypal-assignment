7f6840c6d5fb791edbe9feacfb690a424f8c07ee
carlos alberto <c.morais1507@gmail.com>
2023-08-13 06:43:51 -0300

    :fire: feat: Remove Unused Files

--- app/api/capture-paypal-order/route.ts
+++
@@ -1,9 +0,0 @@
-import { capturePayment } from "@/backend/paypal";
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { orderID } = req.body;
-  const captureData = await capturePayment(orderID);
-  // TODO: store payment information such as the transaction ID
-  res.json(captureData);
-}
--- app/api/create-paypal-order/route.ts
+++
@@ -1,7 +0,0 @@
-import { createOrder } from '@/backend/paypal';
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const order = await createOrder();
-  res.json(order);
-}
